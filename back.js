38778f4925c1de94cecc1e8ce3e70304dddaeb8d
Chaitanyachauhan20 <80047124+Chaitanyachauhan20@users.noreply.github.com>
2021-04-12 22:23:42 -0700

    Add files via upload

---
+++ back.js
@@ -0,0 +1,26 @@
+class Back{
+  constructor(x,y,width,height){
+
+  this.x = x;
+  this.y = y;
+
+  this.width = width;
+  this.height = height;
+  this.image = loadImage("Screenshot (72).png");
+
+  this.body = Bodies.rectangle(this.x,this.y,this.width,this.heights);
+
+  World.add(world,this.body);
+
+  }
+
+  display(){
+      
+   imageMode(CENTER);
+   image(this.image,this.x,this.y,this.width,this.height);
+
+  }
+
+}
+
+
